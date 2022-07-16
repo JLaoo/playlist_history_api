44ba992e5fbc16d9babcdcdf57a0c028a07630bd
JLaoo <jonluo@berkeley.edu>
2022-07-16 11:12:37 -0700

    get env vars

--- app.py
+++ app.py
@@ -3,6 +3,11 @@ import os
 
 app = Flask(__name__)
 
+youtube_api_key = os.getenv('YOUTUBE_API_KEY')
+mongo_user = os.getenv('MONGO_USER')
+mongo_pw = os.getenv('MONGO_PASS')
+mongo_access_str = "mongodb+srv://{}:{}@cluster0.nhgsz.mongodb.net/data?retryWrites=true&w=majority".format(mongo_user, mongo_pw)
+
 @app.route("/")
 def index():
-    return os.getenv('MONGO_USER')
+    mongo_access_str
